2b713e7bbfdfb2fad44fa58ac79847a02ccbe5f8
ysapelie <ysapelie@student.42.fr>
2026-08-26 05:46:15 +0200

    echo_n_fix

--- src/builtins/ft_echo.c
+++ src/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ysapelie <ysapelie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/23 16:54:44 by pilagach          #+#    #+#             */
-/*   Updated: 2026/08/25 23:00:01 by ysapelie         ###   ########.fr       */
+/*   Updated: 2026/08/26 05:45:16 by ysapelie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,24 @@
 int	ft_echo(t_command *cmd)
 {
 	int	i;
+	int	nl_flag;
 
 	i = 1;
-	if (!cmd->argv[1])
+	nl_flag = 1;
+	while (cmd->argv[i] && !ft_strcmp(cmd->argv[i], "-n"))
 	{
-		write(1, "\n", 1);
-		return (0);
+		nl_flag = 0;
+		i++;
 	}
 	while (cmd->argv[i] && cmd->argv[i + 1])
 	{
 		write(cmd->fd_out, cmd->argv[i], ft_strlen(cmd->argv[i]));
+		write(cmd->fd_out, " ", 1);
 		i++;
 	}
-	if (ft_strcmp(cmd->argv[i], "-n"))
-	{
+	if (cmd->argv[i])
 		write(cmd->fd_out, cmd->argv[i], ft_strlen(cmd->argv[i]));
-		write(1, "\n", 1);
-	}
+	if (nl_flag)
+		write(cmd->fd_out, "\n", 1);
 	return (0);
 }
